d0b689db9d1ae86a6e08f73bb0f9995cfb6a7713
Iqbal Al Hafidzu Rahman <92286938+Iqbalalh@users.noreply.github.com>
2021-11-17 13:36:46 +0700

    Add files via upload

---
+++ 2117051019_Iqbal Al Hafidzu Rahman_Pretest4.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+using namespace std;
+int main(){
+	int a;
+	int b;
+	bool c=true;
+	cin>>a;
+
+  if(a==0||a==1) {
+    c=false;
+  }
+  else{
+    for(b=2;b<=a/2;b++){
+      if(a%b==0) {
+        c=false;
+        break;
+      }
+    }
+  }
+  if(c)
+    cout<<a<<" adalah bilangan prima";
+  else
+    cout<<a<<" bukan bilangan prima";
+
+ return 0;
+}
